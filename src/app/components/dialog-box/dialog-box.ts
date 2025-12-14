06efbf73a81bbf97cdc882d4b0ed38c56df1a340
Rahul KR <rahulramakrishnan512@gmail.com>
2025-12-14 20:05:12 +0530

    Active link setup for nav.

--- src/app/components/dialog-box/dialog-box.ts
+++ src/app/components/dialog-box/dialog-box.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output, ChangeDetectionStrategy, Input } from '@angular/core';
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-dialog-box',
   templateUrl: './dialog-box.html',
   styleUrls: ['./dialog-box.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkActive, RouterLinkWithHref],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DialogBox {
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { DialogBox } from '../dialog-box/dialog-box';
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [DialogBox],
+  imports: [DialogBox, RouterLinkActive, RouterLinkWithHref],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
